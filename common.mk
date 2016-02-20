a66e1528aad4882a8068c427378f54dcb491709b
necioerrante <necioerrante@gmail.com>
2016-02-20 05:02:22 +0300

    DUify

--- common.mk
+++ common.mk
@@ -146,8 +146,8 @@ PRODUCT_PACKAGES += \
     libemoji
 
 # APN list
-PRODUCT_COPY_FILES += \
-    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+#PRODUCT_COPY_FILES += \
+#    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
